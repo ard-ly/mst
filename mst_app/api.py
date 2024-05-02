322fa811d53fd8ca08d4b6f942e957b84690c1c0
aseel-gha <aseel.gh@anvilerp.com>
2024-05-02 07:41:27 +0300

    dynamic link

---
+++ mst_app/api.py
@@ -0,0 +1,8 @@
+import frappe
+from frappe.model.document import Document
+from frappe import _, msgprint, throw
+
+# @frappe.whitelist()
+# def set_dynamic_link(doc, method,source):
+#     msgprint("test")
+#     pass
