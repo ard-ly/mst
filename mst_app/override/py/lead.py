0b2039aa5801f06291764375400e30d79010440e
aseel-gha <aseel.gh@anvilerp.com>
2024-07-04 00:31:07 +0400

    Lead

--- mst_app/override/py/lead.py
+++ mst_app/override/py/lead.py
@@ -11,18 +11,18 @@ def get_qualification_value(qualification):
 @frappe.whitelist()
 def get_qualification_marks(doc,method):
     mark = 0 
-    for row in doc.custom_lead_qualification:
-        qualification_and_value_mark = frappe.db.sql(f""" select mark from `tabQualification Table` WHERE qualification='{row.qualification}' AND value='{row.value}' AND parenttype='Lead Qualification Matrix' """,as_dict=1, pluck="mark")
-        if qualification_and_value_mark:
-            mark += qualification_and_value_mark[0]
-            print(mark)
-    
-    qualification_mark = frappe.db.get_value("Lead Qualification Matrix","Lead Qualification Matrix", "qualification_mark")
-    
-    if int(mark) >= int(qualification_mark):
-        doc.qualification_status = 'Qualified'
-        # doc.qualified_on = today()
-    else:
-        doc.qualification_status = 'Unqualified'
-        # doc.qualified_on = today()
-       
+    if doc.custom_lead_qualification:
+        for row in doc.custom_lead_qualification:
+            qualification_and_value_mark = frappe.db.sql(f""" select mark from `tabQualification Table` WHERE qualification='{row.qualification}' AND value='{row.value}' AND parenttype='Lead Qualification Matrix' """,as_dict=1, pluck="mark")
+            if qualification_and_value_mark:
+                mark += qualification_and_value_mark[0]
+                print(mark)
+        
+        qualification_mark = frappe.db.get_value("Lead Qualification Matrix","Lead Qualification Matrix", "qualification_mark")
+        
+        if int(mark) >= int(qualification_mark):
+            doc.qualification_status = 'Qualified'
+            # doc.qualified_on = today()
+        else:
+            doc.qualification_status = 'Unqualified'
+            # doc.qualified_on = today()
